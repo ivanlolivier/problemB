b549f530cd1e561e6b48fb106de33093454e25cb
Ivan L'olivier <ivan.lolivier@gmail.com>
2017-11-21 03:47:22 -0300

    Version with continues

--- problem-b.v2.js
+++ problem-b.v2.js
@@ -9,39 +9,47 @@ var rightLength = 0;
 var cookie;
 
 while (line = readline()) {
+
   if (line === '#') {
     print(right[0]);
 
     if (leftLength >= rightLength) {
       right[0] = left.pop();
       leftLength--;
-    } else {
-      right.splice(0, 1);
-      rightLength--;
+      continue;
     }
 
+    right.splice(0, 1);
+    rightLength--;
+    continue;
+  }
+
+  cookie = parseInt(line, 10);
+
+  if (rightLength === 0) {
+    right.push(cookie);
+    rightLength++;
+    continue;
+  }
+
+  if (cookie >= right[0]) {
+    binaryInsert(cookie, right, 0, rightLength - 1);
+    rightLength++;
   } else {
-    cookie = parseInt(line, 10);
+    binaryInsert(cookie, left, 0, leftLength - 1);
+    leftLength++;
+  }
 
-    if (rightLength === 0 || cookie >= right[0]) {
-      binaryInsert(cookie, right, 0, rightLength - 1);
-      rightLength++;
-    } else {
-      binaryInsert(cookie, left, 0, leftLength - 1);
-      leftLength++;
-    }
+  if (leftLength > rightLength) {
+    right.splice(0, 0, left.pop());
+    rightLength++;
+    leftLength--;
+    continue;
+  }
 
-    if (leftLength > rightLength) {
-      right.splice(0, 0, left.pop());
-      rightLength++;
-      leftLength--;
-    }
-
-    if (rightLength >= leftLength + 2) {
-      leftLength = left.push(right[0]);
-      right.splice(0, 1);
-      rightLength--;
-    }
+  if (rightLength >= leftLength + 2) {
+    leftLength = left.push(right.splice(0, 1)[0]);
+    rightLength--;
   }
 }
 
