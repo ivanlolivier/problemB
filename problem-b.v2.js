071d4f2be928ba6225e03b100996038fe62e27f6
Ivan L'olivier <ivan.lolivier@gmail.com>
2017-11-21 03:23:41 -0300

    Complete version

--- problem-b.v2.js
+++ problem-b.v2.js
@@ -1,55 +1,48 @@
 var line;
 
-var rear = [];
-var front = [];
+var right = [];
+var left = [];
+
+var leftLength = 0;
+var rightLength = 0;
 
 var cookie;
-var h;
-var num1 = 0;
-var num2 = 0;
 
 while (line = readline()) {
   if (line === '#') {
-    print(rear.pop());
-    num2--;
+    print(right[0]);
 
-    if (num1 > num2) {
-      //rear.push(front.pop());
-      binaryInsert(front.pop(), rear);
-      num1--;
-      num2++;
+    if (leftLength >= rightLength) {
+      right[0] = left.pop();
+      leftLength--;
+    } else {
+      right.splice(0, 1);
+      rightLength--;
     }
 
   } else {
     cookie = parseInt(line, 10);
 
-    if (num2 === 0 || cookie >= rear[rear.length - 1]) {
-      //rear.push(cookie);
-      binaryInsert(cookie, rear);
-      num2++;
+    if (rightLength === 0 || cookie >= right[0]) {
+      binaryInsert(cookie, right, 0, rightLength - 1);
+      rightLength++;
     } else {
-      //front.push(cookie);
-      binaryInsert(cookie, front);
-      num1++;
+      binaryInsert(cookie, left, 0, leftLength - 1);
+      leftLength++;
     }
-    if (num1 > num2) {
-      //rear.push(front.pop());
-      binaryInsert(front.pop(), rear);
-      num2++;
-      num1--;
+
+    if (leftLength > rightLength) {
+      right.splice(0, 0, left.pop());
+      rightLength++;
+      leftLength--;
     }
-    if (num2 >= num1 + 2) {
-      front.push(rear.pop());
-      binaryInsert(rear.pop(), front);
-      num1++;
-      num2--;
+
+    if (rightLength >= leftLength + 2) {
+      leftLength = left.push(right[0]);
+      right.splice(0, 1);
+      rightLength--;
     }
   }
-
-  print('rear: ');
-  printArray(rear);
-  print('front: ');
-  printArray(front);
 }
 
 function binaryInsert(value, array, start, end) {
@@ -71,8 +64,4 @@ function binaryInsert(value, array, start, end) {
   if (value > array[middleIndex]) {
     return binaryInsert(value, array, middleIndex + 1, end);
   }
-}
-
-function printArray(array) {
-  print('[' + array.join(', ') + ']');
 }
